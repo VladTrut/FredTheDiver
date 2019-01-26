c9efebd2480a1aff8574bb31128da1add023c045
SylvainCapart <sylvain.capart@cfa-afti.fr>
2019-01-26 23:02:02 +0100

    oxygen 1

---
+++ Assets/Project/Scripts/OxygenMgt.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenMgt : MonoBehaviour
+{
+
+
+    [SerializeField] private AirBarMgt m_AirBarUI;
+    private bool m_SwimMode = false;
+    private bool m_Swimming = false;
+    [SerializeField] private int m_CurrentOxygen;
+    [SerializeField] private int m_MaxOxygen;
+
+    public List<ParticleCollisionEvent> collisionEvents;
+    [SerializeField] private float m_OxygenDecreaseRate;
+    [SerializeField] private float m_OxygenIncreaseRate;
+    [SerializeField] private float m_PassiveOxygenLossRate;
+
+    [SerializeField] private float m_SurfaceY = -1f;
+
+
+
+    public int CurrentOxygen
+    {
+        get
+        {
+            return m_CurrentOxygen;
+        }
+        set
+        {
+            m_CurrentOxygen = value;
+        }
+    }
+
+    public int MaxOxygen { get => m_MaxOxygen; set => m_MaxOxygen = value; }
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        CurrentOxygen = MaxOxygen;
+
+        if (m_AirBarUI == null)
+        {
+            AirBarMgt airBarMgt = FindObjectOfType<AirBarMgt>();
+            if (airBarMgt != null)
+                m_AirBarUI = airBarMgt;
+        }
+
+        m_AirBarUI.SetOxygen( CurrentOxygen, MaxOxygen);
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        m_AirBarUI.SetOxygen( CurrentOxygen, MaxOxygen);
+        SurfaceCheck();
+
+    }
+
+    private void SurfaceCheck()
+    {
+
+
+
+        if (transform.position.y < m_SurfaceY)
+        {
+            CancelInvoke("IncreaseOxygen");
+            if (!IsInvoking("PassiveOxygenLoss"))
+                InvokeRepeating("PassiveOxygenLoss", 1f / m_PassiveOxygenLossRate, 1f / m_PassiveOxygenLossRate);
+        }
+        else
+        {
+
+            CancelInvoke("PassiveOxygenLoss");
+            InvokeRepeating("PassiveOxygenLoss", 1f / m_PassiveOxygenLossRate, 1f / m_PassiveOxygenLossRate);
+        }
+
+        
+
+    }
+
+    public void DecreaseOxygen(int value)
+    {
+         CurrentOxygen -= value;
+    }
+
+    void IncreaseOxygen()
+    {
+         CurrentOxygen += 1;
+    }
+
+    void PassiveOxygenLoss()
+    {
+         CurrentOxygen -= 1;
+    }
+
+
+}
