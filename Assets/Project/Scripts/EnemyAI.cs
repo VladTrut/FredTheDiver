ada4cd05cad5818bb51cae4b850fe446b9d3f6df
SylvainCapart <sylvain.capart@cfa-afti.fr>
2019-01-27 13:01:03 +0100

    enemyai change

--- Assets/Project/Scripts/EnemyAI.cs
+++ Assets/Project/Scripts/EnemyAI.cs
@@ -614,6 +614,14 @@ public class EnemyAI : MonoBehaviour
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
 
 
     private void OnCollisionStay(Collision collision)
